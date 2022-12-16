e7d555a8ec91707858acda707bb8a2487c861f05
Pavel Feldman <pavel.feldman@gmail.com>
2022-12-16 13:45:57 -0800

    chore: make main executable

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /**
  * Copyright (c) Microsoft Corporation.
  *
@@ -13,5 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-require('./lib/main.js');
+module.exports = require('./lib/main');
